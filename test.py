ebd07904045b2ab8cd3c19f2bce0750e743d0d3b
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-03-24 12:20:16 -0500

    test with a model

--- test.py
+++ test.py
@@ -1,28 +1,15 @@
+import os
 import tensorflow as tf
 
 
-tf.io.gfile.makedirs("ram://test/inner")
+model = tf.keras.Sequential([tf.keras.Input((1,)), tf.keras.layers.Dense(1)])
+model.compile(loss="mse")
+model.fit(x=[[1]], y=[1])
 
-with tf.io.gfile.GFile("ram://test/inner/test.txt", mode="w") as f:
-    f.write("test")
+model.save("ram://test")
 
-with tf.io.gfile.GFile("ram://test/inner/test.txt", mode="rb") as f:
-    print(f.size())
-
-
-
-
-# import os
-# import tensorflow as tf
-
-
-# model = tf.keras.Sequential(tf.keras.Input((10,)), tf.keras.layers.Dense(1))
-
-# model.save("ram://test")
-
-# for root, _, filenames in tf.io.gfile.walk("ram://test"):
-#     for filename in filenames:
-#         dest_path = os.path.join(root, filename)
-#         with tf.io.gfile.GFile(dest_path, mode="rb") as f:
-#             f.write("test")
-#             assert f.size() == 4
+for root, _, filenames in tf.io.gfile.walk("ram://test"):
+    for filename in filenames:
+        dest_path = os.path.join(root, filename)
+        with tf.io.gfile.GFile(dest_path, mode="rb") as f:
+            print(f.size())
