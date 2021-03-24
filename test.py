4e99cf850336d629a53883ac7207569ab1f84c1d
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-03-24 12:10:08 -0500

    open in rb

--- test.py
+++ test.py
@@ -2,4 +2,24 @@ from tensorflow.io.gfile import GFile
 
 with GFile("ram://test.txt", mode="w") as f:
     f.write("test")
+
+with GFile("ram://test.txt", mode="rb") as f:
     print(f.size())
+
+
+
+
+# import os
+# import tensorflow as tf
+
+
+# model = tf.keras.Sequential(tf.keras.Input((10,)), tf.keras.layers.Dense(1))
+
+# model.save("ram://test")
+
+# for root, _, filenames in tf.io.gfile.walk("ram://test"):
+#     for filename in filenames:
+#         dest_path = os.path.join(root, filename)
+#         with tf.io.gfile.GFile(dest_path, mode="rb") as f:
+#             f.write("test")
+#             assert f.size() == 4
