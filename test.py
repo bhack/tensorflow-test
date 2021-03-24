e7d55bcb508cb90aeab5b642c2740dac09266557
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-03-24 14:16:10 -0500

    use the right character

--- test.py
+++ test.py
@@ -10,9 +10,9 @@ model.save("ram://test")
 
 for root, _, filenames in tf.io.gfile.walk("ram://test"):
     for filename in filenames:
-        if filename[0] == "/":
+        if filename[0] == "\\":
             # on windows
-            print("STRIPPING LEADING /")
+            print("STRIPPING LEADING \\")
             filename = filename[1:]
         path = os.path.join(root, filename)
         print(f"root: {root}")
@@ -21,3 +21,12 @@ for root, _, filenames in tf.io.gfile.walk("ram://test"):
         print("exists: ", tf.io.gfile.exists(path))
         with tf.io.gfile.GFile(path, mode="rb") as f:
             print(f.size())
+
+
+# import tensorflow as tf
+
+
+# tf.io.gfile.makedirs("ram://test")
+# with tf.io.gfile.GFile("ram://test/test.txt", mode="w") as f:
+#     f.write("test")
+# print(tf.io.gfile.walk("ram://test"))
