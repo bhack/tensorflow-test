50432b1c8b221cb678ae053b55f207ec599cff01
bhack <bhack@users.noreply.github.com>
2021-03-26 00:47:33 +0100

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ with tf.io.gfile.GFile("ram://folder/file.txt", mode="w") as f:
 
 for root, _, filenames in tf.io.gfile.walk("ram://folder"):
     print(root)
+    print(filenames)
     for filename in filenames:
         print(filename)
+        print(os.path.join(root, filename))
+        assert tf.io.gfile.exists("ram://folder/file.txt")
         assert tf.io.gfile.exists(os.path.join(root, filename))
