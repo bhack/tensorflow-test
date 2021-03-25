3b0c866a6f2c07a2570c13e3c22b28fba7d88634
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-03-25 18:04:59 -0500

    fix

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import os
 import tensorflow as tf
 
 
-tf.gfile.makedir("ram://folder")
-with tf.gfile.Gfile("ram://folder/file.txt", mode="w") as f:
+tf.io.gfile.makedirs("ram://folder")
+with tf.io.gfile.GFile("ram://folder/file.txt", mode="w") as f:
     f.write("data")
 
 for root, _, filenames in tf.io.gfile.walk("ram://folder"):
