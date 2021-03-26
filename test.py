7ac38edce579b6e75bf1c63c91b391950cb0862a
bhack <bhack@users.noreply.github.com>
2021-03-26 03:59:59 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 tf.io.gfile.makedirs("ram://test/inner")
 
-with tf.io.gfile.GFile("ram://test\\inner\\file.txt", mode="w") as f:
+with tf.io.gfile.GFile("ram://test\\\\inner\\\\file.txt", mode="w") as f:
     f.write("data")
 
 for root, _, filenames in tf.io.gfile.walk("ram://test"):
